9e86b6dd2d6e8593d1e02cc45b26840c273341a8
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-07-08 12:51:29 +0200

    fix webpack config + generated dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,25 @@
-
-
 const path = require('path');
-const cloneDeep = require('lodash.clonedeep');
-const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
-const defaults = {
-    context: path.resolve(__dirname, 'src'),
-
-    entry: './js-tracker.js',
-
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        library: 'TrackerAsset',
-        libraryTarget: 'umd',
-        filename: 'js-tracker.bundle.js'
-    },
-
-    node: {
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty'
-    },
-
-    plugins: []
+module.exports = {
+  entry: './src/js-tracker.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js-tracker.bundle.js'
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: "/test/",
+        exclude: "/node_modules/",
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 };
-
-const minified = cloneDeep(defaults); // Use const for minified since it's not reassigned
-minified.plugins.push(new webpack.optimize.UglifyJsPlugin());
-minified.output.filename = 'js-tracker.bundle.min.js';
-
-module.exports = [defaults, minified];
