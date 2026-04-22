8834925dea036a38bacca2a0052ccb87e064e848
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2026-04-22 12:07:48 +0800

    update bundle

--- dist/js-tracker.bundle.cjs
+++ dist/js-tracker.bundle.cjs
@@ -3513,7 +3513,7 @@ class Statement {
         this.actor = actor;
         this.verb = new VerbStatement(verbId, defaultURI);
         this.defaultURI = defaultURI;
-        if((!isUri(objectType) && (objectType === 'interaction' || objectType === 'cmi.interaction'))) {
+        if(objectType === ALL.ACTIVITYTYPES.CMI_INTERACTION) {
             this.object = new InteractionObjectStatement(objectId, objectType, this.defaultURI);
         } else {
             this.object = new ObjectStatement(objectId, objectType, this.defaultURI);
