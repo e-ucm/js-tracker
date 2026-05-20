0b9969d328d0abe58ed00a87c6ada5753d6791df
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2026-05-20 08:22:14 +0800

    fix: change import to export for module declarations in declarations.d.ts

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,3 +1,3 @@
 declare module 'js-tracker' {
-    import { JSTracker, JSScormTracker, LRSTracker, SeriousGameTracker } from './src/js-tracker';
+    export { JSTracker, JSScormTracker, LRSTracker, SeriousGameTracker } from './src/js-tracker';
 }
